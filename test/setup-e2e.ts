be219a7db4af958a064dcb4b68ad995db53c4064
Leo <leomiasiro@gmail.com>
2024-10-28 19:48:38 -0300

    fix

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,32 +1,17 @@
-import { randomUUID } from 'node:crypto';
 import { execSync } from 'node:child_process';
-import {prisma} from "../src/lib/prisma";
-import * as console from "node:console";
-
-
-function generateUniqueDatabaseURL(schemaId: string) {
-    if (!process.env.DATABASE_URL) {
-        throw new Error('Please provider a DATABASE_URL environment variable.');
-    }
-
-    const url = new URL(process.env.DATABASE_URL);
-
-    url.searchParams.set('schema', schemaId);
-    return url.toString();
-}
-
-const schemaId = randomUUID();
+import { prisma } from 'src/lib/prisma';
 
 beforeAll(async () => {
-    const databaseURL = generateUniqueDatabaseURL(schemaId);
-
-    process.env.DATABASE_URL = databaseURL;
 
     execSync('npx prisma migrate deploy');
 });
 
 afterAll(async () => {
-    await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`);
-    await prisma.$disconnect();
+    try {
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "test" CASCADE`);
+        await prisma.$disconnect()
+    } catch (error) {
+        console.error(error)
+    }
 
 });
